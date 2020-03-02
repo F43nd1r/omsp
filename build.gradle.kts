ce7e7b86c7bbe2da4d8c02d2bdf08c626cca8109
f43nd1r <lukas.morawietz@gmail.com>
2020-03-02 02:01:41 +0100

    add publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 plugins {
     kotlin("jvm") version "1.3.61"
-    //kotlin("kapt") version "1.3.61"
+    `maven-publish`
 }
 
-group = "com.faendir"
-version = "1.0-SNAPSHOT"
+group = "com.faendir.omsp"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
@@ -31,5 +31,55 @@ tasks {
     }
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
+    }
+}
+
+val sourcesJar by tasks.registering(Jar::class) {
+    classifier = "sources"
+    from(sourceSets.main.get().allSource)
+}
+
+publishing {
+    repositories {
+        mavenLocal()
+        maven {
+            setUrl("https://api.bintray.com/maven/f43nd1r/maven/omsp/;publish=1")
+            name = "bintray"
+            credentials {
+                username = findProperty("artifactoryUser") as String?
+                password = findProperty("artifactoryApiKey") as String?
+            }
+        }
+    }
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+            artifact(sourcesJar.get())
+            pom {
+                name.set("omsp")
+                description.set("Opus Magnum Solution Parser")
+                url.set("https://github.com/F43nd1r/omsp")
+
+                scm {
+                    connection.set("scm:git:https://github.com/F43nd1r/omsp.git")
+                    developerConnection.set("scm:git:git@github.com:F43nd1r/omsp.git")
+                    url.set("https://github.com/F43nd1r/omsp.git")
+                }
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        distribution.set("repo")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("f43nd1r")
+                        name.set("Lukas Morawietz")
+                    }
+                }
+            }
+        }
     }
 }
