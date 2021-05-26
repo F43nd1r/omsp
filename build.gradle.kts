aa96dcdc3e5c48a693c4c914a08c6b0bb1614e42
f43nd1r <account+github@faendir.com>
2021-05-26 16:12:08 +0200

    2.0.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.faendir.om"
-version = "2.0.1"
+version = "2.0.2"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/com/faendir/om/parser/solution/model/part/GlyphType.kt
+++ src/main/kotlin/com/faendir/om/parser/solution/model/part/GlyphType.kt
@@ -1,19 +1,22 @@
 package com.faendir.om.parser.solution.model.part
 
-enum class GlyphType(internal val key: String) {
-    BONDER("bonder"),
-    MULTI_BONDER("bonder-speed"),
-    TRIPLEX_BONDER("bonder-prisma"),
-    UNBONDER("unbonder"),
-    CALCIFICATION("glyph-calcification"),
-    DUPLICATION("glyph-duplication"),
-    PROJECTION("glyph-projection"),
-    PURIFICATION("glyph-purification"),
-    ANIMISMUS("glyph-life-and-death"),
-    DISPOSAL("glyph-disposal"),
-    UNIFICATION("glyph-unification"),
-    DISPERSION("glyph-dispersion"),
-    EQUILIBRIUM("glyph-marker");
+import com.faendir.om.parser.solution.model.Position
+import com.faendir.om.parser.solution.model.to
+
+enum class GlyphType(internal val key: String, val shape : Set<Position>) {
+    BONDER("bonder", setOf(0 to 0, 1 to 0)),
+    MULTI_BONDER("bonder-speed", setOf(0 to 0, 1 to 0, -1 to 1, 0 to -1)),
+    TRIPLEX_BONDER("bonder-prisma", setOf(0 to 0, 1 to 0, 0 to 1)),
+    UNBONDER("unbonder", setOf(0 to 0, 1 to 0)),
+    CALCIFICATION("glyph-calcification", setOf(0 to 0)),
+    DUPLICATION("glyph-duplication", setOf(0 to 0, 1 to 0)),
+    PROJECTION("glyph-projection", setOf(0 to 0, 1 to 0)),
+    PURIFICATION("glyph-purification", setOf(0 to 0, 1 to 0, 0 to 1)),
+    ANIMISMUS("glyph-life-and-death", setOf(0 to 0, 1 to 0, 0 to 1, 1 to -1)),
+    DISPOSAL("glyph-disposal", setOf(0 to 0, 1 to 0, 0 to 1, -1 to 1, -1 to 0, 0 to -1, 1 to -1)),
+    UNIFICATION("glyph-unification", setOf(0 to 0, 0 to 1, -1 to 1, 0 to -1, 1 to -1)),
+    DISPERSION("glyph-dispersion", setOf(0 to 0, 1 to 0, -1 to 0, 0 to -1, 1 to -1)),
+    EQUILIBRIUM("glyph-marker", setOf(0 to 0));
 
     companion object {
         fun fromString(key: String): GlyphType? {
