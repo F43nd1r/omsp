07ae423c088a0908563c6f33c2d4bee67761ee8e
f43nd1r <account+github@faendir.com>
2021-05-26 19:21:42 +0200

    2.0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "com.faendir.om"
-version = "2.0.2"
+version = "2.0.3"
 
 repositories {
     mavenCentral()
--- src/main/kotlin/com/faendir/om/parser/solution/model/part/Conduit.kt
+++ src/main/kotlin/com/faendir/om/parser/solution/model/part/Conduit.kt
@@ -6,7 +6,7 @@ data class Conduit(
     override var position: Position,
     override var rotation: Int,
     var id: Int,
-    internal var positions: List<Position>
+    var positions: List<Position>
 ) : Part() {
     override val name = "pipe"
 }
